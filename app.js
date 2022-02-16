c54796bc6948197c23c4a38adf1ff170889e248e
Karim Mannai <46463907+Zgon7@users.noreply.github.com>
2022-02-16 10:07:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
-module.exports = app;
+app.listen(8000);
